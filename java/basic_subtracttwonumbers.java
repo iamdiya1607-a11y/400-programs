5b06ad597e433c742e4d594d308f9e2d8d5ce619
iamdiya1607-a11y <iamdiya1607@gmail.com>
2026-08-31 19:26:43 +0530

    03_Create basic_subtracttwonumbers.java

---
+++ java/basic_subtracttwonumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class subtracttwonumbers {
+  public static void main(String args[])
+  {
+    Scanner sc = new Scanner (System.in);
+
+  System.out.print("enter first number");
+    int a = sc.nextInt();
+    System.out.print("enter second number");
+    int b = sc.nextInt();
+
+  int diffrence = a - b;
+
+  System.out.println("Diffrence="+Diffrence);
+  }
+}
